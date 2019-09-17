4d02ed82591beb076710e0423db23f18767ee86b
rogeruwayezu <rogeruwayezu@gmail.com>
2019-09-17 08:07:42 +0200

    first commit

---
+++ instance/config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY = '58aed921d71acba6bbb12787dbef553c'
+SECRET_KEY = '<Flask WTF Secret Key>'
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='58aed921d71acba6bbb12787dbef553c'
+export SECRET_KEY='<Flask WTF Secret Key>'
+
+python3.6 manage.py server
+
